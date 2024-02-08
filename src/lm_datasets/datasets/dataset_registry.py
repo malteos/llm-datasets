989d9afe4419a73fcc796c81b08393e95f95ccf4
lflage <flage.lucas@gmail.com>
2024-02-08 18:26:48 +0100

    dataset: added file for PleiasNews dataset
    added main class file and function to auto generate classes
    Added this function to the registry

--- src/lm_datasets/datasets/dataset_registry.py
+++ src/lm_datasets/datasets/dataset_registry.py
@@ -3,6 +3,7 @@ from .multilingual.wikimedia import get_wikimedia_auto_classes
 from .multilingual.colossal_oscar import get_colossal_oscar_auto_classes
 from .multilingual.eurlex import get_eurlex_auto_classes
 from .multilingual.legal_mc4 import get_legal_mc4_auto_classes
+from .fr.pleiasnews import get_pleias_news_auto_classes
 from .code.starcoder import get_auto_starcoder_classes
 from .nl.sonar import get_sonar_classes
 from .en.pile_of_law import get_pile_of_law_auto_classes
@@ -183,6 +184,7 @@ def get_registered_dataset_classes(
             + get_colossal_oscar_auto_classes()
             + get_auto_starcoder_classes()
             + get_pile_of_law_auto_classes()
+            + get_pleias_news_auto_classes()
         )
 
     if extra_dataset_classes:
